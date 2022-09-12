3734b2c8b98e5822f7707c37296030e09b7fd340
Putra Fajar <putrautalls@gmail.com>
2022-09-12 20:11:57 +0700

    added registerDTO validation to the project

--- dto/registerDTO.go
+++ dto/registerDTO.go
@@ -2,13 +2,13 @@ package dto
 
 type RegisterApplicantDTO struct {
 	Username string `json:"username" form:"username" binding:"required"`
-	Email    string `json:"email" form:"email" binding:"required"`
-	Password string `json:"password" form:"password" binding:"required"`
+	Email    string `json:"email" form:"email" binding:"required,email"`
+	Password string `json:"password" form:"password" binding:"required,min=6"`
 }
 
 type RegisterEmployeeDTO struct {
 	Username string `json:"username" form:"username" binding:"required"`
-	Email    string `json:"email" form:"email" binding:"required"`
+	Email    string `json:"email" form:"email" binding:"required,email"`
 	Role     string `json:"role" form:"role" binding:"required"`
-	Password string `json:"password" form:"password" binding:"required"`
+	Password string `json:"password" form:"password" binding:"required,min=6"`
 }
