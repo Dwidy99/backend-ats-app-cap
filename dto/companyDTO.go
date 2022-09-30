8b726d2e87b8a83b5bc1021f54e81091784967ae
Putra Fajar <putrautalls@gmail.com>
2022-09-30 09:41:30 +0700

    delete company entity, DTO, service and repository

--- dto/companyDTO.go
+++
@@ -1,9 +0,0 @@
-package dto
-
-type CompanyDTO struct {
-	Name    string `json:"name" form:"name" binding:"required"`
-	Email   string `json:"email" form:"email" binding:"required"`
-	Address string `json:"address" form:"address" binding:"required"`
-	Contact string `json:"contact" form:"contact" binding:"required"`
-	Website string `json:"website" form:"website" binding:"required"`
-}
